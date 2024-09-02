41e2d2b8db70e01b515ba699a1f2b9a1195f9a1d
Saumya <131542228+saumyakhark@users.noreply.github.com>
2024-09-02 23:03:34 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+place = input("Enter the place: ")
+name = input("Enter another person's name: ")
+activity = input("Enter what the person will be doing: ")
+reaction = input("Enter your reaction: ")
+
+print(f"I went to {place} yesterday and I saw {name} doing {activity}. I was so {reaction} that I returned Home.")
